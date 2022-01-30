d7e617cf293477f2684ab38a87780fb2c2a41316
adibradu <adrian.bradu@gmail.com>
2022-01-30 10:36:18 +0200

    parralax effect on service page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require("cors");
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-const nodemailer = require("nodemailer");
-require('dotenv').config()
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const app = express();
 
@@ -14,37 +14,35 @@ app.get('/*', function (req, res) {
 });
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/contact', async (req, res) => {
+  let { name, email, message } = req.body;
 
-  let {name, email, message} = req.body
-
-  let parcel = (
+  let parcel =
     `<p>Name: ${name}</p>` +
     `<p>Email: ${email}</p>` +
-    `<p>Message: ${message}</p>`
-  )
+    `<p>Message: ${message}</p>`;
 
   const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST, 
+    host: process.env.MAIL_HOST,
     port: 465,
     secure: true,
     auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
     },
     tls: {
-        rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   });
 
   transport.verify((error) => {
     if (error) {
       console.log(error);
     } else {
-      console.log("Ready to send email");
+      console.log('Ready to send email');
     }
   });
 
@@ -53,17 +51,16 @@ app.post('/contact', async (req, res) => {
     to: email,
     bcc: process.env.BCC,
     subject: `Inquiry from web`,
-    html: parcel
-  }
+    html: parcel,
+  };
 
-  transport.sendMail(mailOptions, function(error, info){
+  transport.sendMail(mailOptions, function (error, info) {
     if (error) {
-        console.log('Error: ',error);
+      console.log('Error: ', error);
     } else {
-        console.log('Email sent: ' + info.response);
+      console.log('Email sent: ' + info.response);
     }
   });
-
 });
 
-app.listen(process.env.PORT, () => console.log("ISR server running..."));
+app.listen(process.env.PORT, () => console.log('ISR server running...'));
